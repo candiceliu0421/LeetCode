efe7651a9ef341b8b7f6d4dbe28739935477f538
candice <candice102403006@gmail.com>
2018-08-14 14:19:53 +0800

    finished懶人法

---
+++ Python/ ImplementstrStr().py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        # Python String.find()方法
+        return haystack.find(needle)
