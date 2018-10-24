ae472b1f4a53383fd9d3be3b828d28290947be6b
candiceliu0421 <candice102403006@gmail.com>
2018-10-24 11:52:29 +0800

    0(n) solution -> compile error , use O(logn)

---
+++ C++/searchInsert.cpp
@@ -0,0 +1,17 @@
+# use binary search O(logN) solution 
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0, right=nums.size()-1;
+        while(left<right) {
+            int mid = left + (right-left)/2;
+            if(target==nums[mid]) 
+                return mid;
+            else if(target<nums[mid])
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return (target > nums[left]) ? left + 1 : left;
+    }
+};
