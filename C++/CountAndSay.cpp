30a5c8c6ed835f7eb0e31b56c416d3297c114712
candiceliu0421 <candice102403006@gmail.com>
2018-11-13 14:35:30 +0800

    finished

---
+++ C++/CountAndSay.cpp
@@ -0,0 +1,26 @@
+# Easy38 Count and Say
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n<1) return "";
+        string ret="1";
+        for(int i=2;i<=n;i++){
+            string temp="";
+            int count=1;
+            char prev=ret[0];
+            for(int j=1;j<ret.size();j++){
+                if(ret[j]==prev) count++;
+                else{
+                    temp=temp+to_string(count);
+                    temp.push_back(prev);
+                    count=1;
+                    prev=ret[j];
+                }
+            }
+            temp=temp+to_string(count);
+            temp.push_back(prev);
+            ret = temp;
+        }
+        return ret; 
+    }
+};
